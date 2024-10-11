20155f111c70baec1c407eaca97b633d549c534c
njiah <nsdl1411@gmail.com>
2024-10-11 20:35:35 +0100

    navbar

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,7 +3,6 @@ function Experience() {
   return (
     <div className="white" id="experience">
       <h1>Experience</h1>
-      <p>Experience is the name everyone gives to their mistakes.</p>
     </div>
   );
 }
