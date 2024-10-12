f6d9b141c28a0ccd324aac255082c12f6c693c6c
njiah <nsdl1411@gmail.com>
2024-10-12 17:42:57 +0100

    nav bar cleanup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import { Link } from 'react-scroll';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { gsap } from 'gsap';
@@ -7,68 +7,73 @@ const VeritcalNav = (props) => {
     gsap.registerPlugin(ScrollTrigger);
     const navbarRef = useRef(null);
 
-    //const aboutNav = document.getElementById("aboutNav");
-    //const projectsNav = document.getElementById("projectsNav");
-
     const [aboutcolorChange, aboutsetColorChange] = useState(false);
     const [projectscolorChange, projectssetColorChange] = useState(false);
     const [educationcolorChange, educationsetColorChange] = useState(false);
     const [experiencecolorChange, experiencesetColorChange] = useState(false);
     const [contactcolorChange, contactsetColorChange] = useState(false);
 
-    const sections = document.querySelectorAll("section");
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                console.log(entry.target.id);
-                switch(entry.target.id) {
-                case "about": 
-                    aboutsetColorChange(true);
-                    projectssetColorChange(false);
-                    educationsetColorChange(false);
-                    experiencesetColorChange(false);
-                    contactsetColorChange(false);
-                    break;
-                case "projects": 
-                    aboutsetColorChange(false);
-                    projectssetColorChange(true);
-                    educationsetColorChange(false);
-                    experiencesetColorChange(false);
-                    contactsetColorChange(false);
-                    break;
-                case "education": 
-                    aboutsetColorChange(false);
-                    projectssetColorChange(false);
-                    educationsetColorChange(true);
-                    experiencesetColorChange(false);
-                    contactsetColorChange(false);
-                    break;
-                case "experience": 
-                    aboutsetColorChange(false);
-                    projectssetColorChange(false);
-                    educationsetColorChange(false);
-                    experiencesetColorChange(true);
-                    contactsetColorChange(false);
-                    break;
-                default: 
-                    aboutsetColorChange(false);
-                    projectssetColorChange(false);
-                    educationsetColorChange(false);
-                    experiencesetColorChange(false);
-                    contactsetColorChange(true);
-                    break;
+    useEffect(() => {
+
+        const sections = document.querySelectorAll("section");
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    console.log(entry.target.id);
+                    switch(entry.target.id) {
+                    case "about": 
+                        aboutsetColorChange(true);
+                        projectssetColorChange(false);
+                        educationsetColorChange(false);
+                        experiencesetColorChange(false);
+                        contactsetColorChange(false);
+                        break;
+                    case "projects": 
+                        aboutsetColorChange(false);
+                        projectssetColorChange(true);
+                        educationsetColorChange(false);
+                        experiencesetColorChange(false);
+                        contactsetColorChange(false);
+                        break;
+                    case "education": 
+                        aboutsetColorChange(false);
+                        projectssetColorChange(false);
+                        educationsetColorChange(true);
+                        experiencesetColorChange(false);
+                        contactsetColorChange(false);
+                        break;
+                    case "experience": 
+                        aboutsetColorChange(false);
+                        projectssetColorChange(false);
+                        educationsetColorChange(false);
+                        experiencesetColorChange(true);
+                        contactsetColorChange(false);
+                        break;
+                    default: 
+                        aboutsetColorChange(false);
+                        projectssetColorChange(false);
+                        educationsetColorChange(false);
+                        experiencesetColorChange(false);
+                        contactsetColorChange(true);
+                        break;
+                    }
                 }
             }
-        }
-    );
-    }, {
-        root: null,
-        threshold: 0.5
-    });
-    sections.forEach(section => {
-        observer.observe(section);
-    });
-    
+        );
+        }, {
+            root: null,
+            threshold: 0.5
+        });
+        sections.forEach((section) => {
+            observer.observe(section);
+        });
+        return () => {
+            sections.forEach((section) => {
+                observer.unobserve(section);
+
+            });
+        };
+    }, []);
         
     return (
         <div className="Navbar" ref={{navbarRef}} id="navbar">
